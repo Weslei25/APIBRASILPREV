5298977d704da39f459f3e1ad93e41fecc0a20f1
Weslei25 <wesleisantos25@hotmail.com>
2023-01-16 23:43:15 -0300

    teste

--- produto/tests/tests_models.py
+++ produto/tests/tests_models.py
@@ -22,27 +22,12 @@ class ClienteTesteCase(TestCase):
             expiracaoDeVenda = "2023-01-16",
             valorMinimoAporteInicial= 2899.5,
             valorMinimoAporteExtra = 2899.5, 
-            idadeDeEntrada =18, 
+            idadeDeEntrada = 18, 
             idadeDeSaida =  60,
             carenciaInicialDeResgate = 60, 
             carenciaEntreResgates = 30 
         )
     
-    def test_retorno_name(self):
-        cliente = ClienteModel.objects.get(id=self.cliente.id)
-        self.assertEquals(cliente.__str__(), 'Weslei')
-
-    def test_cpf_cliente(self):
-        cliente = ClienteModel.objects.filter(id=self.cliente.id).values()[0]
-        self.assertEquals(len(cliente['cpf']), 11)
-    
-    def test_email_cliente(self):
-        cliente = ClienteModel.objects.filter(id=self.cliente.id).values()[0]
-        self.assertEquals(len(cliente['sexo']), 1)
-    
-    def test_renda_float(self):
-        cliente = ClienteModel.objects.filter(id=self.cliente.id).values()[0]
-        self.assertNotEqual(type(cliente['rendaMensal']), float)
 
     def test_assert_produto(self):
         produto = ProdutoModel.objects.filter(id=self.produto.id).values()[0]
@@ -50,3 +35,4 @@ class ClienteTesteCase(TestCase):
         self.assertEquals(produto['nome'].__str__(), self.produto.nome)
         self.assertTrue(produto['id'] == self.produto.id)
         self.assertTrue(produto['valorMinimoAporteInicial'] > 1000)
+        self.assertTrue(produto['idadeDeEntrada'] >= 18)
